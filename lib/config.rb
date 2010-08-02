0feae410ded15deb8e1ca9d8bd941fe4f5a1fe6d
skim <skim.la@gmail.com>
2010-08-02 12:05:25 -0500

    oops, now enabled 3x3 and 4x4 in config

--- lib/config.rb
+++ lib/config.rb
@@ -3,8 +3,8 @@ TTT::CONFIG.boards = {
   '4x4' => { :active => true, :cache => :mongo, :on => "4x4.jpg", :off => "4x4_dim.jpg" }
 }
 
- TTT::CONFIG.boards['3x3'][:active] = false
- TTT::CONFIG.boards['4x4'][:active] = false
+# TTT::CONFIG.boards['3x3'][:active] = false
+# TTT::CONFIG.boards['4x4'][:active] = false
 
 TTT::CONFIG.players = {
   'human' => { :value => "human",       :on => "player_human.jpg",    :off => "player_human_dim.jpg" },
