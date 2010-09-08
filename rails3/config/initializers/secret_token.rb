4d58ab20171a3fd9d5006d5b7bdb067b95b967eb
skim <skim.la@gmail.com>
2010-09-08 10:09:09 -0500

    new rails3 app

---
+++ rails3/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3::Application.config.secret_token = 'd3196e6119f15de56c1023e73df0f146595e8f0a0a4cf99af775fd927a8c20ba2e190872fbf0b746e013718f481763005de9af664008cc4a8b9d08bf64ac4d7c'
