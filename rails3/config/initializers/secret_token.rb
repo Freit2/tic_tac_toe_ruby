a088e2dd0d69eb95c82f03cad95a89e23762cc0b
skim <skim.la@gmail.com>
2010-09-13 11:35:32 -0500

    recreated rails app with skip-gemfile, skip-activerecord, skip-testunit, skip-prototype

--- rails3/config/initializers/secret_token.rb
+++ rails3/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails3::Application.config.secret_token = 'd3196e6119f15de56c1023e73df0f146595e8f0a0a4cf99af775fd927a8c20ba2e190872fbf0b746e013718f481763005de9af664008cc4a8b9d08bf64ac4d7c'
+Rails3::Application.config.secret_token = 'cdd99fea2bba8b4bbda658c37222a7dad66ce7b7b339a9d4fc3a24b01ad7bf1201d7a30f83d7e3120c578974e46488e8f6af4f43819f7e9c11e0bd41ec9414ea'
